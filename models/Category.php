ec72244a20169d0265008b115f6ac02bdf77e9bd
taras <tgamanov@gmail.com>
2016-05-23 00:00:27 +0300

    CRUD for Product and Category tables have been created

---
+++ models/Category.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tbl_category".
+ *
+ * @property integer $id
+ * @property string $name
+ *
+ * @property TblProduct[] $tblProducts
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'tbl_category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTblProducts()
+    {
+        return $this->hasMany(TblProduct::className(), ['category_id' => 'id']);
+    }
+}
---
+++ models/Product.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use app\models\Category;
+
+/**
+ * This is the model class for table "tbl_product".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $category_id
+ * @property string $price
+ *
+ * @property Category $category
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'tbl_product';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'category_id', 'price'], 'required'],
+            [['category_id'], 'integer'],
+            [['price'], 'number'],
+            [['name'], 'string', 'max' => 255],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Category::className(), 'targetAttribute' => ['category_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'category_id' => 'Category ID',
+            'price' => 'Price',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::className(), ['id' => 'category_id']);
+    }
+}
