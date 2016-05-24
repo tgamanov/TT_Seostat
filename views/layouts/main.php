6a041758cc17744e4a9a057ee54ef9605647ab15
taras <tgamanov@gmail.com>
2016-05-24 12:15:05 +0300

    Main page changed, link from menu to products has been added

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -37,20 +37,8 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post', ['class' => 'navbar-form'])
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+            ['label' => 'Products', 'url' => ['/product/index']],
+            
         ],
     ]);
     NavBar::end();
